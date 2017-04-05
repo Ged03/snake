f52a978d00b005a8d89a612762bab1e8bc300a1b
ged <nick.cheb2@mail.ru>
2017-04-05 17:55:41 +0500

    Рисуем точки на экране.

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -16,10 +16,10 @@ namespace Snake
 
             Draw(x1, y1, sym1);
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
+            x1 = 4;
+            y1 = 5;
+            sym1 = '#';
+            Draw(x1, y1, sym1);
 
             Console.ReadLine();
         }
